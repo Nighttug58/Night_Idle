f726931fb494c9648e2649c511bdabf6d7523da6
Nighttug58 <makrobion@gmail.com>
2026-09-14 14:40:01 +0200

    feat: load game feel feedback before core

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const BUILD = "20260914-stats1";
+  const BUILD = "20260914-feel1";
 
   function loadScript(src) {
     return new Promise((resolve, reject) => {
@@ -25,6 +25,12 @@
       await loadScript("js/stats.js");
     } catch (error) {
       console.warn("[Night Idle] Statistiques indisponibles, poursuite du boot.", error);
+    }
+
+    try {
+      await loadScript("js/feel.js");
+    } catch (error) {
+      console.warn("[Night Idle] Feedback visuel/sonore indisponible, poursuite du boot.", error);
     }
 
     try {
